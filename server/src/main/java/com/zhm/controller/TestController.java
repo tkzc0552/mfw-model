4bd962a3ff622c15e444351592270b818cf7ffab
zhaohongming <hongming.zhao@seedeer.com>
2020-01-02 14:22:28 +0800

    sddd

---
+++ server/src/main/java/com/zhm/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.zhm.controller;
+
+import com.zhm.annotation.TokenFilter;
+import com.zhm.dto.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 赵红明 on 2020/1/2.
+ */
+@Api(value = "测试",description = "这是一个测试的接口，调试是否可用")
+@RestController
+@RequestMapping(value = "/api/test")
+public class TestController {
+    @TokenFilter
+    @ApiOperation(value = "测试能否返回(不用拦截的)",notes = "这是备注",httpMethod = "GET")
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public Result test(){
+        return Result.sendSuccess("调试通过");
+    }
+
+}
